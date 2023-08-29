401049adcb44650fcc9c8b4e667c764f97cce522
Petr "Stone" Hracek <phracek@redhat.com>
2023-08-29 16:34:25 +0200

    Fix directory issue
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/unit/test_email.py
+++ tests/unit/test_email.py
@@ -20,57 +20,60 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from pathlib import Path
+
+
 from betka.constants import TEMPLATES
 from betka.utils import text_from_template
 
 class TestEmails:
 
+    def setup_method(self):
+        self.container = "postgresql-container"
+        self.commit_sha = "123456789"
+        self.downstream_name = "postgresql-12"
+        self.template_dir = Path(__file__).parent.parent.parent / "files/home/templates"
     def test_create_mergerequest_message(self):
-        container = "postgresql-container"
         status = "created"
-        commit_sha = "123456789"
-        downstream_name = "postgresql-12"
         mr_number = 1
         betka_dict = {
             "status": status,
-            "commit": commit_sha,
-            "upstream_repo": f"https://github.com/sclorg/{container}",
+            "commit": self.commit_sha,
+            "upstream_repo": f"https://github.com/sclorg/{self.container}",
             "namespace": "redhat/rhel/containers",
             "mr_number": mr_number,
             "gitlab": "https://gitlab.com",
-            "image": downstream_name
+            "image": self.downstream_name
         }
+
         test_message = text_from_template(
-            template_dir=TEMPLATES,
+            template_dir=self.template_dir,
             template_filename="email_template",
             template_data=betka_dict
         )
         assert test_message
         assert f"Betka {status} merge request:" in test_message
-        assert f"branch https://github.com/sclorg/{container} and synced" in test_message
-        assert f"https://gitlab.com/redhat/rhel/containers/{downstream_name}/-/merge_requests/{mr_number}" in test_message
+        assert f"branch https://github.com/sclorg/{self.container} and synced" in test_message
+        assert f"https://gitlab.com/redhat/rhel/containers/{self.downstream_name}/-/merge_requests/{mr_number}" in test_message
 
     def test_updated_mergerequest_message(self):
-        container = "postgresql-container"
         status = "updated"
-        commit_sha = "123456789"
         mr_number = 2
-        downstream_name = "postgresql-12"
         betka_dict = {
             "status": status,
-            "commit": commit_sha,
-            "upstream_repo": f"https://github.com/sclorg/{container}",
+            "commit": self.commit_sha,
+            "upstream_repo": f"https://github.com/sclorg/{self.container}",
             "namespace": "redhat/rhel/containers",
             "mr_number": mr_number,
             "gitlab": "https://gitlab.com",
-            "image": downstream_name
+            "image": self.downstream_name
         }
         test_message = text_from_template(
-            template_dir=TEMPLATES,
+            template_dir=self.template_dir,
             template_filename="email_template",
             template_data=betka_dict
         )
         assert test_message
         assert f"Betka {status} merge request:" in test_message
-        assert f"branch https://github.com/sclorg/{container} and synced" in test_message
-        assert f"https://gitlab.com/redhat/rhel/containers/{downstream_name}/-/merge_requests/{mr_number}" in test_message
+        assert f"branch https://github.com/sclorg/{self.container} and synced" in test_message
+        assert f"https://gitlab.com/redhat/rhel/containers/{self.downstream_name}/-/merge_requests/{mr_number}" in test_message
