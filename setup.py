4f26f701b2631808ee8c7e6ff8e1bca657d56206
Petr "Stone" Hracek <phracek@redhat.com>
2024-09-18 11:35:12 +0200

    Add more debug output
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements():
 
 setup(
     name="betka",
-    version="0.7.14",
+    version="0.7.15",
     packages=find_packages(exclude=["examples", "tests"]),
     url="https://github.com/sclorg/betka",
     license="GPLv3+",
