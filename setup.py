43cbd9cff90de60d23412a23337097760de61903
Petr "Stone" Hracek <phracek@redhat.com>
2024-11-18 14:23:10 +0100

    Fix typo in reading betka_configuration file. 'use_gitlab_fork' does not exist. 'use_gitlab_forks' is proper variable.
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements():
 
 setup(
     name="betka",
-    version="0.9.5",
+    version="0.9.6",
     packages=find_packages(exclude=["examples", "tests"]),
     url="https://github.com/sclorg/betka",
     license="GPLv3+",
